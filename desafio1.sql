9114ee97d5582229e94003c996d8b5ae40622136
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-04-12 16:11:14 -0300

    Alteração no código do Banco de Dados "SpotifyClone".

--- desafio1.sql
+++ desafio1.sql
@@ -1,29 +1,30 @@
+DROP DATABASE IF EXISTS SpotifyClone;
 CREATE SCHEMA SpotifyClone;
 USE SpotifyClone;
-CREATE TABLE artist (
+CREATE TABLE SpotifyClone.artist (
   artist_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   artist_name VARCHAR(100) NOT NULL UNIQUE
 );
-CREATE TABLE `signature` (
+CREATE TABLE SpotifyClone.`signature` (
   signature_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   signature_type VARCHAR(50) NOT NULL,
   price DECIMAL(10, 2) NOT NULL UNIQUE
 );
-CREATE TABLE album (
+CREATE TABLE SpotifyClone.album (
   album_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   album_name VARCHAR(100) NOT NULL UNIQUE,
   release_year INT UNSIGNED NOT NULL,
   artist_id INT UNSIGNED NOT NULL,
   FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
 );
-CREATE TABLE songs (
+CREATE TABLE SpotifyClone.songs (
   song_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   song_name VARCHAR(100) NOT NULL UNIQUE,
   duration_seconds INT UNSIGNED NOT NULL,
   album_id INT UNSIGNED NOT NULL,
   FOREIGN KEY (album_id) REFERENCES album (album_id)
 );
-CREATE TABLE `user` (
+CREATE TABLE SpotifyClone.`user` (
   user_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_name VARCHAR(100) NOT NULL,
   age INT UNSIGNED NOT NULL,
@@ -31,7 +32,7 @@ CREATE TABLE `user` (
   user_signature_date DATE NOT NULL,
   FOREIGN KEY (signature_id) REFERENCES `signature` (signature_id)
 );
-CREATE TABLE music_history (
+CREATE TABLE SpotifyClone.music_history (
   history_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   song_id INT UNSIGNED NOT NULL,
   user_id INT UNSIGNED NOT NULL,
@@ -39,7 +40,7 @@ CREATE TABLE music_history (
   FOREIGN KEY (song_id) REFERENCES songs (song_id),
   FOREIGN KEY (user_id) REFERENCES `user` (user_id)
 );
-CREATE TABLE `following` (
+CREATE TABLE SpotifyClone.`following` (
   user_id INT UNSIGNED NOT NULL,
   artist_id INT UNSIGNED NOT NULL
 );
